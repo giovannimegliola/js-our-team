300ed650effffba79f3e19ca7b6fdb2889f74567
giovannimegliola <giovanni.megliola@hotmail.it>
2023-10-23 16:08:45 +0200

    added cards in cardsContainer

--- js/script.js
+++ js/script.js
@@ -41,3 +41,20 @@ const team = [
 for (let key in team) {
   console.log (team[key].name + " " + team[key].role + " " + team[key].picture);
 }
+
+const cards = document.getElementById("cardsContainer");
+
+for (let i = 0; i < team.length; i++){
+
+  let member = team[i];
+
+  let card = ` 
+  <div class="card">
+  <img src="./img/${member.picture}"></img>
+  <h3>${member.name}</h3>
+  <h5>${member.role}</h5>
+  </div>
+  `
+
+  cards.innerHTML += card;
+}
